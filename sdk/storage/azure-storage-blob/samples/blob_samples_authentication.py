3196d367de9485272bf44e7ddc28493abab0e2fa
FacerAin <ywsong.dev@kakao.com>
2023-09-01 00:54:07 +0900

    chore: typo in storage-blob sample

--- sdk/storage/azure-storage-blob/samples/blob_samples_authentication.py
+++ sdk/storage/azure-storage-blob/samples/blob_samples_authentication.py
@@ -17,7 +17,7 @@ USAGE:
     python blob_samples_authentication.py
     Set the environment variables with your own values before running the sample:
     1) AZURE_STORAGE_CONNECTION_STRING - the connection string to your storage account
-    2) OAUTH_STORAGE_ACCOUNT_NAME - the oath storage account name
+    2) OAUTH_STORAGE_ACCOUNT_NAME - the oauth storage account name
     3) AZURE_STORAGE_ACCOUNT_NAME - the name of the storage account
     4) AZURE_STORAGE_ACCESS_KEY - the storage account access key
     5) ACTIVE_DIRECTORY_APPLICATION_ID - Azure Active Directory application ID
@@ -27,13 +27,10 @@ USAGE:
 
 import os
 
+
 class AuthSamples(object):
-    url = "https://{}.blob.core.windows.net".format(
-        os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
-    )
-    oauth_url = "https://{}.blob.core.windows.net".format(
-        os.getenv("OAUTH_STORAGE_ACCOUNT_NAME")
-    )
+    url = "https://{}.blob.core.windows.net".format(os.getenv("AZURE_STORAGE_ACCOUNT_NAME"))
+    oauth_url = "https://{}.blob.core.windows.net".format(os.getenv("OAUTH_STORAGE_ACCOUNT_NAME"))
 
     connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING")
     shared_access_key = os.getenv("AZURE_STORAGE_ACCESS_KEY")
@@ -44,19 +41,22 @@ class AuthSamples(object):
     def auth_connection_string(self):
         # [START auth_from_connection_string]
         from azure.storage.blob import BlobServiceClient
+
         blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
         # [END auth_from_connection_string]
 
         # [START auth_from_connection_string_container]
         from azure.storage.blob import ContainerClient
-        container_client = ContainerClient.from_connection_string(
-            self.connection_string, container_name="mycontainer")
+
+        container_client = ContainerClient.from_connection_string(self.connection_string, container_name="mycontainer")
         # [END auth_from_connection_string_container]
 
         # [START auth_from_connection_string_blob]
         from azure.storage.blob import BlobClient
+
         blob_client = BlobClient.from_connection_string(
-            self.connection_string, container_name="mycontainer", blob_name="blobname.txt")
+            self.connection_string, container_name="mycontainer", blob_name="blobname.txt"
+        )
         # [END auth_from_connection_string_blob]
 
         # Get account information for the Blob Service
@@ -65,6 +65,7 @@ class AuthSamples(object):
     def auth_shared_key(self):
         # [START create_blob_service_client]
         from azure.storage.blob import BlobServiceClient
+
         blob_service_client = BlobServiceClient(account_url=self.url, credential=self.shared_access_key)
         # [END create_blob_service_client]
 
@@ -74,6 +75,7 @@ class AuthSamples(object):
     def auth_blob_url(self):
         # [START create_blob_client]
         from azure.storage.blob import BlobClient
+
         blob_client = BlobClient.from_blob_url(blob_url="https://account.blob.core.windows.net/container/blob-name")
         # [END create_blob_client]
 
@@ -88,14 +90,16 @@ class AuthSamples(object):
         # [START create_blob_service_client_oauth]
         # Get a token credential for authentication
         from azure.identity import ClientSecretCredential
+
         token_credential = ClientSecretCredential(
             self.active_directory_tenant_id,
             self.active_directory_application_id,
-            self.active_directory_application_secret
+            self.active_directory_application_secret,
         )
 
         # Instantiate a BlobServiceClient using a token credential
         from azure.storage.blob import BlobServiceClient
+
         blob_service_client = BlobServiceClient(account_url=self.oauth_url, credential=token_credential)
         # [END create_blob_service_client_oauth]
 
@@ -105,6 +109,7 @@ class AuthSamples(object):
     def auth_shared_access_signature(self):
         # Instantiate a BlobServiceClient using a connection string
         from azure.storage.blob import BlobServiceClient
+
         blob_service_client = BlobServiceClient.from_connection_string(self.connection_string)
 
         # [START create_sas_token]
@@ -117,7 +122,7 @@ class AuthSamples(object):
             account_key=blob_service_client.credential.account_key,
             resource_types=ResourceTypes(object=True),
             permission=AccountSasPermissions(read=True),
-            expiry=datetime.utcnow() + timedelta(hours=1)
+            expiry=datetime.utcnow() + timedelta(hours=1),
         )
         # [END create_sas_token]
 
@@ -129,20 +134,20 @@ class AuthSamples(object):
         # Alternately, one can specify the AZURE_TENANT_ID, AZURE_CLIENT_ID, and AZURE_CLIENT_SECRET to use the EnvironmentCredentialClass.
         # The docs above specify all mechanisms which the defaultCredential internally support.
         from azure.identity import DefaultAzureCredential
+
         default_credential = DefaultAzureCredential()
 
         # Instantiate a BlobServiceClient using a token credential
         from azure.storage.blob import BlobServiceClient
-        blob_service_client = BlobServiceClient(
-            account_url=self.oauth_url,
-            credential=default_credential
-        )
+
+        blob_service_client = BlobServiceClient(account_url=self.oauth_url, credential=default_credential)
         # [END create_blob_service_client_oauth_default_credential]
 
         # Get account information for the Blob Service
         account_info = blob_service_client.get_service_properties()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     sample = AuthSamples()
     sample.auth_connection_string()
     sample.auth_active_directory()
